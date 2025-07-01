2fb0942bbde5161386afbfea64d84c842edd1e28
DavidLC578 <davidlatt2002@gmail.com>
2025-07-01 00:54:44 -0400

    add dependencies

--- shell.nix
+++ shell.nix
@@ -5,6 +5,10 @@ pkgs.mkShell {
   buildInputs = [
     pkgs.nodejs_20  # Or the version you use
     pkgs.pnpm       # Or pkgs.nodePackages.npm if you use npm
+    pkgs.git
+    pkgs.python3
+    pkgs.gcc
+    pkgs.openssl
   ];
 
   shellHook = ''
